e7b061a03075d5aa8fdc89e0eeedf57f724101a5
Zanie Blue <contact@zanie.dev>
2023-12-13 16:21:48 -0600

    Show unsatisfied requirements in `view` (#5)

--- src/packse/view.py
+++ src/packse/view.py
@@ -57,10 +57,12 @@ def dependency_tree(scenario: Scenario):
         versions = scenario.packages[package].versions
 
         pointers = [tee] * (len(versions) - 1) + [last]
+        satisfied = False
         for pointer, version in zip(pointers, versions):
             if for_requirement and not for_requirement.specifier.contains(version):
                 continue
 
+            satisfied = True
             message = "satisfied by " if for_requirement else ""
             yield prefix + pointer + message + f"{package}-{version}"
 
@@ -68,6 +70,9 @@ def dependency_tree(scenario: Scenario):
             yield from render_requirements_for(
                 package, version, prefix=prefix + extension
             )
+
+        if for_requirement and not satisfied:
+            yield prefix + last + "unsatisfied"
 
     def render_requirements_for(package: str, version: str, prefix: str = ""):
         requirements = list(scenario.packages[package].versions[version].requires)
@@ -84,6 +89,8 @@ def dependency_tree(scenario: Scenario):
                     prefix=prefix + extension,
                     for_requirement=parsed_requirement,
                 )
+            else:
+                yield prefix + space + last + "unsatisfied"
 
     for line in render_package_versions(scenario.root):
         buffer += line + "\n"
